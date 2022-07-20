900c8d4ba9457ff862a8b90f6cce34b68f309ec2
MF-Ahmed <itsfarhan@outlook.com>
2022-07-20 15:01:35 +0200

    remamed a folder
    remaming of a folder

--- PyQT5/form the book guy/all_the_source_code/code _Backup/layout_1.py
+++
@@ -1,9 +0,0 @@
-class MainWindow(QMainWindow):
-
-    def __init__(self, *args, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
-        
-        self.setWindowTitle("My Awesome App")
-
-        widget = Color('red')
-        self.setCentralWidget(widget)
--- PyQT5/form the book guy/all_the_source_code/code _Backup/multithreading_5.py
+++
@@ -1,9 +0,0 @@
-def execute_this_fn(self):
-    print("Hello!")
-
-def oh_no(self):
-    # Pass the function to execute
-    worker = Worker(self.execute_this_fn) # Any other args, kwargs are passed to the run function
-
-    # Execute
-    threadpool.start(worker)
